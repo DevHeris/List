fc6a52cbcc767be6d8336a7ca51f4933dd99dfe2
DevHeris <badairoinioluwa578@gmail.com>
2024-01-13 05:45:01 +0200

    Callback based functions using promises

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const util = require("util");
 
-// This is a function
+// ================== CALLBACK ERROR HANDLING
+
+// // This is a function
+// fs.readdir(process.cwd(), (err, filenames) => {
+//   // EITHER
+//   // err === an error object, which means something went wrong
+//   // OR
+//   // err === null, which means everything is OK
+
+//   if (err) {
+//     // error handling code if there is an error and everything is not okay
+//     console.log(err);
+//     // throw new Error(err);
+//   }
+// });
+
+// -------------------------------------------------
+// =================== WORKING WITH lstat() FUNCTION
+// -------------------------------------------------
+
+//   -------------------------SOLUTION #1 - Callback based solution
+// fs.readdir(process.cwd(), (err, filenames) => {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   const allStats = Array(filenames.length).fill(null);
+
+//   for (let filename of filenames) {
+//     const index = filenames.indexOf(filename);
+
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         console.log(err);
+//       }
+
+//       allStats[index] = stats;
+
+//       const ready = allStats.every((stats) => {
+//         return stats;
+//       });
+
+//       if (ready) {
+//         allStats.forEach((stats, index) => {
+//           console.log(filenames[index], stats.isFile());
+//         });
+//       }
+//     });
+//   }
+// });
+
+//   -------------------------SOLUTION #2 - Callback based functions using Promises
+
 fs.readdir(process.cwd(), (err, filenames) => {
-  // EITHER
-  // err === an error object, which means something went wrong
-  // OR
-  // err === null, which means everything is OK
-
-  if (err) {
-    // error handling code if there is an error and everything is not okay
-    console.log(err);
-    // throw new Error(err);
-  }
-
-  // Callback based solution
-  const allStats = Array(filenames.length).fill(null);
-
-  for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
-
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-
-      allStats[index] = stats;
-
-      const ready = allStats.every((stats) => {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
-  }
+  if (err) console.log(err);
 });
+
+// METHOD #1 OF WRAPPING lsat() INSIDE A PROMISE
+
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) reject(err);
+//       resolve(stats);
+//     });
+//   });
+// };
+
+// METHOD #2 OF WRAPPING lsat() INSIDE A PROMISE - (Promisify Function automatically) - Using a module called "Utils"
+
+// const lstat = util.promisify(fs.lstat);
+
+// METHOD #3 OF WRAPPING lsat() INSIDE A PROMISE
+
+const { lstat } = fs.promises;
