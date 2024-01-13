c8725737d1be38d836e3ccee328de7faceff82b7
DevHeris <badairoinioluwa578@gmail.com>
2024-01-13 22:53:06 +0200

    Console Logs with Colors

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 "# List"
 
 ### Working with Nodejs standard library and getting used to some modules like the "fs" module
+
+## lstat() implementation method
+
+### By wrapping the lstat call witha promise,using async/await + the promise.all helper method to process the lstat calls all at once
+
+### chalk npm package for giving colors to differentiate between files and folders
+
+#### Red color for folders and Blue Color for file
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const util = require("util");
+// const fs = require("fs");
+import fs from "fs";
+// const util = require("util");
+import utils from "util";
+import chalk from "chalk";
 
 // ================== CALLBACK ERROR HANDLING
 
@@ -56,9 +59,20 @@ const util = require("util");
 
 //   -------------------------SOLUTION #2 - Callback based functions using Promises
 
-fs.readdir(process.cwd(), (err, filenames) => {
-  if (err) console.log(err);
-});
+// This stats implememtaion method isn't the best option just like the Solution 1 also isn't the best. The best is the solution #3 which you will see after this solution
+
+// fs.readdir(process.cwd(), async (err, filenames) => {
+//   if (err) console.log(err);
+
+//   for (const filename of filenames) {
+//     try {
+//       const stats = await lstat(filename);
+//       console.log(filename, stats.isFile());
+//     } catch (error) {
+//       error;
+//     }
+//   }
+// });
 
 // METHOD #1 OF WRAPPING lsat() INSIDE A PROMISE
 
@@ -76,5 +90,29 @@ fs.readdir(process.cwd(), (err, filenames) => {
 // const lstat = util.promisify(fs.lstat);
 
 // METHOD #3 OF WRAPPING lsat() INSIDE A PROMISE
-
 const { lstat } = fs.promises;
+
+//   -------------------------SOLUTION #3 - wrap the lstat call with a promise, use async/await + the Promise.all helper method to process lstat calls all at once
+
+// Kind of a combination of method 1 and 2
+
+// THE RIGHT WAY
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) console.log(err);
+
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename);
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (const stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(chalk.blue(filenames[index]));
+    } else {
+      console.log(chalk.red(filenames[index]));
+    }
+  }
+});
